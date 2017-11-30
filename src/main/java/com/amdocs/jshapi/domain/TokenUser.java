0d8045464ea61efae26379d0a0e8c4c838e658e2
seiferxx <seifer.ch@gmail.com>
2017-11-30 21:47:28 +0000

    Added activities on TokenUser class for security

---
+++ src/main/java/com/amdocs/jshapi/domain/TokenUser.java
@@ -0,0 +1,29 @@
+package com.amdocs.jshapi.domain;
+
+public class TokenUser {
+    
+    /**
+     * 
+     * create pojo with the following attributes and create repository
+     * 
+     * 
+     * account.getNickname(), 
+	account.getPassword(), 
+	account.getEnabled(), 
+	account.getEnabled(), 
+	account.getEnabled(), 
+	account.getEnabled(),
+	account.getGrantedAuthorities()
+	
+	 boolean	isAccountNonExpired() 
+          Indicates whether the user's account has expired.
+ boolean	isAccountNonLocked() 
+          Indicates whether the user is locked or unlocked.
+ boolean	isCredentialsNonExpired() 
+          Indicates whether the user's credentials (password) has expired.
+ boolean	isEnabled() 
+          Indicates whether the user is enabled or disabled.
+     */
+    
+    
+}
--- src/main/java/com/amdocs/jshapi/security/AuthorizationServerConfiguration.java
+++ src/main/java/com/amdocs/jshapi/security/AuthorizationServerConfiguration.java
@@ -1,4 +1,14 @@
 package com.amdocs.jshapi.security;
 
-public class AuthorizationServerConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter{
+    
+    private TokenStore tokenStore = new InMemoryTokenStore();
 }
