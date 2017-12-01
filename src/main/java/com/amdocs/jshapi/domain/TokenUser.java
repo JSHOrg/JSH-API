a8f449f50b898400cfdcfc48a9c11f5df583e137
LuisH <luis1016@live.com.mx>
2017-12-01 16:48:28 +0000

    JPA TokenUser class.

--- src/main/java/com/amdocs/jshapi/domain/TokenUser.java
+++ src/main/java/com/amdocs/jshapi/domain/TokenUser.java
@@ -1,29 +1,55 @@
 package com.amdocs.jshapi.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "TOKENUSER")
 public class TokenUser {
-    
-    /**
-     * 
-     * create pojo with the following attributes and create repository
-     * 
-     * 
-     * account.getNickname(), 
-	account.getPassword(), 
-	account.getEnabled(), 
-	account.getEnabled(), 
-	account.getEnabled(), 
-	account.getEnabled(),
-	account.getGrantedAuthorities()
+		
+	@Id
+	@Column(name = "ID_USER")
+	private String Id_user;
+	private String Name;
+	private String Password;
+	private boolean Enabled;
 	
-	 boolean	isAccountNonExpired() 
-          Indicates whether the user's account has expired.
- boolean	isAccountNonLocked() 
-          Indicates whether the user is locked or unlocked.
- boolean	isCredentialsNonExpired() 
-          Indicates whether the user's credentials (password) has expired.
- boolean	isEnabled() 
-          Indicates whether the user is enabled or disabled.
-     */
-    
-    
+	public String getId_user() {
+		return Id_user;
+	}
+	public void setId_user(String id_user) {
+		Id_user = id_user;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public boolean isEnabled() {
+		return Enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		Enabled = enabled;
+	}	
+	
+	public TokenUser() {
+		
+	}
+	public TokenUser(String id_user, String name, String password,
+			boolean enabled) {
+		this.Id_user = id_user;
+		this.Name = name;
+		this.Password = password;
+		this.Enabled = enabled;
+	}
 }
