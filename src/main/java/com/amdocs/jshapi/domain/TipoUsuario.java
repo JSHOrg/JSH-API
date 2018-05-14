c03e429bb2bb5ba548be241c287de8752ea80cc1
roberto-gg-mx <roberto.gg.mx@gmail.com>
2018-05-14 01:39:57 -0500

    Adding TipoUsuario

---
+++ src/main/java/com/amdocs/jshapi/domain/TipoUsuario.java
@@ -0,0 +1,64 @@
+package com.amdocs.jshapi.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tipousuario")
+public class TipoUsuario {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private String nombre;
+
+	@Column
+	private String descripcion;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
