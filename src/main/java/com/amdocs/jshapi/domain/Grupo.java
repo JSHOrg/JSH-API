b3f4dd7026c24fdacc8f1c58dd2f0d2270ff603b
roberto-gg-mx <roberto.gg.mx@gmail.com>
2018-07-27 09:19:33 -0500

    Adding Grupo

--- src/main/java/com/amdocs/jshapi/domain/Grupo.java
+++ src/main/java/com/amdocs/jshapi/domain/Grupo.java
@@ -3,14 +3,13 @@
  */
 package com.amdocs.jshapi.domain;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
@@ -18,7 +17,7 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="servicio")
+@Table(name="grupo")
 public class Grupo {
 
 	@Id
@@ -29,12 +28,83 @@ public class Grupo {
 	@Column
     private String comunidad;
 	
-	@OneToMany(fetch=FetchType.EAGER)
-	private List<TrabajadorSocial> trabajadorAsignado;
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(referencedColumnName = "idtrabajadorsocial", name="idtrabajadorsocial")
+	private TrabajadorSocial trabajadorAsignado;
 	
 	@Column
     private int integrantes;
 	
 	@Column
     private float progreso;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the comunidad
+	 */
+	public String getComunidad() {
+		return comunidad;
+	}
+
+	/**
+	 * @param comunidad the comunidad to set
+	 */
+	public void setComunidad(String comunidad) {
+		this.comunidad = comunidad;
+	}
+
+	/**
+	 * @return the trabajadorAsignado
+	 */
+	public TrabajadorSocial getTrabajadorAsignado() {
+		return trabajadorAsignado;
+	}
+
+	/**
+	 * @param trabajadorAsignado the trabajadorAsignado to set
+	 */
+	public void setTrabajadorAsignado(TrabajadorSocial trabajadorAsignado) {
+		this.trabajadorAsignado = trabajadorAsignado;
+	}
+
+	/**
+	 * @return the integrantes
+	 */
+	public int getIntegrantes() {
+		return integrantes;
+	}
+
+	/**
+	 * @param integrantes the integrantes to set
+	 */
+	public void setIntegrantes(int integrantes) {
+		this.integrantes = integrantes;
+	}
+
+	/**
+	 * @return the progreso
+	 */
+	public float getProgreso() {
+		return progreso;
+	}
+
+	/**
+	 * @param progreso the progreso to set
+	 */
+	public void setProgreso(float progreso) {
+		this.progreso = progreso;
+	}
 }
